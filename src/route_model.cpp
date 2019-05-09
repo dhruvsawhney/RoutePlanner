6c5db5e760ebf7890ac2f4726ca048a909fa3be1
Dhruv Sawhney <dhruvsawhne96@gmail.com>
2019-05-09 20:20:49 +0000

    code

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -10,16 +10,16 @@ RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
   }
   
   void RouteModel::CreateNodeToRoadHashmap() {
-    for(auto road: Roads()){
-
+	
+    for (auto road: Roads()){
+     
       if(road.type != Model::Road::Type::Footway){
-       for(int node_idx: Ways()[road.way].nodes){
-         
-         if(node_to_road.find(node_idx) == node_to_road.end()){
-            node_to_road[node_idx] = std::vector<const Model::Road *> lst;
-         }
-           node_to_road[node_idx].push_back(road);
-       } 
+        for(int node_idx: Ways()[road.way].nodes){
+          if(node_to_road.find(node_idx) != node_to_road.end()){
+           node_to_road[node_idx] = std::vector<const Model::Road*> lst;
+          }
+          node_to_road[node_idx].push_back(road);
+        }
       }
     }
     return;
